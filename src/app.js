72ffc71e5bfdb13b2ed09794cb9f6cf5f36d9925
sebastiaandeckx <sebastiaan.deckx@gmail.com>
2025-04-03 12:18:40 +0200

    path dependency corrected

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRoutes);
 

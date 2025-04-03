0eef494022ded8ca71dbe5dccd8b9d38dc728604
sebastiaandeckx <sebastiaan.deckx@gmail.com>
2025-04-03 10:29:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', indexRoutes);
+app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);

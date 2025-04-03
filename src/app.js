027169d6b007f14c6fa54e4a24771ac5597cd587
sebastiaandeckx <sebastiaan.deckx@gmail.com>
2025-04-03 12:25:42 +0200

    V02

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/api');
+const indexRoutes = require('./routes/index');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', indexRouter);
 
 app.use(bodyParser.json());
 
